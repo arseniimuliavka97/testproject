74df70aab094f9589b97ee049b94267da7b04aee
arseniimuliavka97 <142666084+arseniimuliavka97@users.noreply.github.com>
2024-05-14 18:16:58 +0300

    Add files via upload

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,23 +9,5 @@ class Employee extends Model
 {
     use HasFactory;
     
-    //protected $table = 'employees';
-    
-    //protected $primary_key = 'employee_id';
-    
-    //protected $table = 'employee_salary';
-    
-    //protected $table = 'employee_age';
-    
-    //protected $table = 'profile_image';
-    
-    //protected $guarded = [];
-    
     public $employees;
-    
-    //public function department() {
-        //return this->belongsTo(Department::class);
-    //}
-    
-    //$employees = Employee::all(); 
 }
